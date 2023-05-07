6614462916a704c370c2f6583c71b00210c1aa92
DariaClem <stefana.daria.sd@gmail.com>
2023-05-08 01:47:17 +0300

    Initial commit

---
+++ resources/file.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+
+    document.getElementsByClassName("confettiButton")[0].onclick = function () {
+        let canvas = document.createElement("canvas");
+        canvas.width = 500;
+        canvas.height = 500;
+        let container = document.getElementsByClassName("buttons")[0];
+        container.appendChild(canvas);
+
+        console.log(container);
+
+        let confetti_button = confetti.create(canvas);
+        confetti_button({
+            particleCount: 200,
+            spread: 200,
+            startVelocity: 15,
+            scalar: 0.9,
+            ticks: 90
+        }).then(() => container.removeChild(canvas));
+    }
+
+}
