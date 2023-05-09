3b97c6488ee025af5f1800034fb608e2e698aa24
DariaClem <stefana.daria.sd@gmail.com>
2023-05-09 16:45:19 +0300

    Multiple language configuration

--- resources/file.js
+++ resources/file.js
@@ -19,4 +19,59 @@ window.onload = function () {
         }).then(() => container.removeChild(canvas));
     }
 
+    document.getElementsByClassName("confettiButton")[1].onclick = function () {
+        let canvas = document.createElement("canvas");
+
+        canvas.width = 450;
+        canvas.height = 450;
+
+        let container = document.getElementsByClassName("buttons")[1];
+        container.appendChild(canvas);
+
+        let confetti_button = confetti.create(canvas);
+        confetti_button({
+            particleCount: 200,
+            spread: 200,
+            startVelocity: 15,
+            scalar: 0.9,
+            ticks: 90
+        }).then(() => container.removeChild(canvas));
+    }
+
+    let roSmall = document.querySelectorAll("small.ro");
+    let enSmall = document.querySelectorAll("small.en");
+
+    for (let ro of roSmall) {
+        ro.onclick = function() {
+
+            for(let otherRo of roSmall) {
+                otherRo.style.color = "var(--lightCyan)";
+            }
+
+            document.getElementsByClassName("content-english")[0].style.display = "none";
+            document.getElementsByClassName("content-romanian")[0].style.display = "block";
+
+            for (let en of enSmall) {
+                en.style.color = "white";
+            }
+        }
+    }
+
+    for (let en of enSmall) {
+        en.onclick = function () {
+
+            for(let otherEn of enSmall) {
+                otherEn.style.color = "var(--lightCyan)";
+            }
+
+            document.getElementsByClassName("content-romanian")[0].style.display = "none";
+            document.getElementsByClassName("content-english")[0].style.display = "block";
+
+            for (let ro of roSmall) {
+                ro.style.color = "white";
+            }
+        }
+    }
+
+
 }
